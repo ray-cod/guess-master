e625dd6070a4a93aaa5b1e43340bb8f95fecbdde
ray-cod <raimilassissi8@gmail.com>
2025-09-03 14:20:34 +0200

    Created game screen view.

--- src/main/java/com/guessing/gamemaster/controllers/GameViewController.java
+++ src/main/java/com/guessing/gamemaster/controllers/GameViewController.java
@@ -1,4 +1,16 @@
 package com.guessing.gamemaster.controllers;
 
+import javafx.fxml.FXML;
+import javafx.scene.control.Label;
+import javafx.scene.control.ListView;
+
 public class GameViewController {
+
+    @FXML
+    private ListView<String> guessHistory;
+
+    @FXML
+    public void initialize() {
+        guessHistory.setPlaceholder(new Label("No guesses yet..."));
+    }
 }
