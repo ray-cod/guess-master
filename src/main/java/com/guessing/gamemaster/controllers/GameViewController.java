9b047735ca5372ce746d2d60bd357774a997709f
ray-cod <raimilassissi8@gmail.com>
2025-09-07 11:10:33 +0200

    Set basic game functionality.

--- src/main/java/com/guessing/gamemaster/controllers/GameViewController.java
+++ src/main/java/com/guessing/gamemaster/controllers/GameViewController.java
@@ -1,8 +1,13 @@
 package com.guessing.gamemaster.controllers;
 
+import com.guessing.gamemaster.services.GameService;
+import com.guessing.gamemaster.utils.GuessResult;
 import javafx.fxml.FXML;
 import javafx.scene.control.Label;
 import javafx.scene.control.ListView;
+import javafx.scene.control.TextField;
+
+import java.time.LocalTime;
 
 public class GameViewController {
 
@@ -11,6 +16,19 @@ public class GameViewController {
     @FXML private Label roundLabel;
     @FXML private Label instructionLabel;
     @FXML private Label attemptLabel;
+    @FXML private TextField guessField;
+    @FXML private Label feedbackLabel;
+    @FXML private Label scoreLabel;
+
+    /*==== Game properties ===*/
+    private int target;
+    private int guess;
+    private int score = 0;
+    private GuessResult guessResult;
+    private int totalAttempts;
+    private int playerAttempts = 0;
+    private int rangeLimit;
+    private LocalTime playTime = LocalTime.of(0, 0, 50);
 
     @FXML
     public void initialize() {
@@ -23,5 +41,28 @@ public class GameViewController {
         instructionLabel.setText("Instruction: Guess a number between 1 and " + range + ".");
         attemptLabel.setText("1 / " + attempts);
 
+        // ==== Setting game properties ===
+        this.target = GameService.generateTargetNumber(1, range);
+        this.totalAttempts = attempts;
+        this.rangeLimit = range;
+    }
+
+    @FXML
+    public void onGuessClicked() {
+        playerAttempts += 1;
+        guess = Integer.parseInt(guessField.getText());
+
+        guessResult = GameService.checkGuess(guess, target);
+        if (guessResult.isCorrect()){
+            score += GameService.calculateScore(playerAttempts, playTime, rangeLimit);
+            feedbackLabel.setText(guessResult.message());
+        } else {
+            score -= 2;
+            feedbackLabel.setText(guessResult.message());
+        }
+
+        scoreLabel.setText("Score: " + score);
+        attemptLabel.setText((playerAttempts + 1) + " / " + totalAttempts);
+        guessField.setText("");
     }
 }
